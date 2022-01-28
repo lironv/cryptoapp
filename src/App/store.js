ebfa2e4b7bb2d329b565722b38efedacfbb3ca18
lironv <lironvaknin13@gmail.com>
2022-01-28 15:44:29 +0200

    version 1, unfinished

---
+++ src/App/store.js
@@ -0,0 +1,12 @@
+import { configureStore } from '@reduxjs/toolkit';
+
+import { cryptoApi } from '../Services/cryptoApi';
+
+
+
+export default configureStore({
+    reducer: {
+        [cryptoApi.reducerPath]: cryptoApi.reducer,
+    },
+
+});
